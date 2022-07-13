eda7c4dec30c667d801dce93d10d3ea6bfd8743a
PESolut <jahaadpetty@pursuit.org>
2022-07-13 12:22:50 -0400

    Problems with getDinosaursAliveMya

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,38 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getLongestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getLongestDinosaur(dinosaurs) {}
+function getLongestDinosaur(dinosaurs) {
+  if (!dinosaurs.length){ // edge case
+    return {}
+  }
+  // we gonna be using the lengthinmeters key
+  let longestLength = dinosaurs[0].lengthInMeters // initalize var to hold first dino length ( accumulator )
+  let longestName = dinosaurs[0].name// store the name of the dinosaur 
+  const longestDino = {}// initalize var to hold an empty object ( our return )
+  // t(hrough each iteration; check the length in meters; store the first length as the record holder)
+  for (const dino of dinosaurs){
+    // if length in meters are higher then our record holder variable
+    // if it is; update the variable to our accumulator as the highest.
+    if(dino.lengthInMeters > longestLength){
+      longestLength = dino.lengthInMeters
+      longestName = dino.name
+    }
+  }
+  
+  // if it is not; go to the next iteration.
+
+  // once a record is found ( or whole object of arrays is iterated through;)
+  // convert from length of meters to feet by mult by 3.281
+  longestLength *= 3.281
+  
+  longestDino[longestName] = longestLength
+  // add the name of the longestName to the {} as a key,  and have the length as the value.
+  // return the object storing our dino
+  return longestDino
+
+  //iterate over the dinosaurs
+
+}
 
 /**
  * getDinosaurDescription()
@@ -38,13 +69,29 @@ function getLongestDinosaur(dinosaurs) {}
  * @returns {string} A detailed description of the dinosaur.
  *
  * EXAMPLE:
- *  getDinosaurDescription(dinosaurs, "U9vuZmgKwUr");
+ *  getDinosaurDescraiption(dinosaurs, "U9vuZmgKwUr");
  *  //> "Xenoceratops (ZEE-no-SEH-ruh-tops)\nXenoceratops had horns and a bony frill with elaborate ornamentation of projections, knobs, and spikes. It lived in the Early Cretaceous period, over 77.5 million years ago."
  *
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) {
+  // declare var to hold the dino that we find that matches the id
+  let dino = false
+  // iterate through dinosaurs using for of
+  // compare the parma id number against each iteration of the dinos id
+  // if they are a match, update dino var to hold found dino info
+  // return string formated to have this dinos info
+  for (i = 0; i < dinosaurs.length; i++){
+    if(id === dinosaurs[i].dinosaurId){
+      dino = dinosaurs[i]
+      return `${dino.name} (${dino.pronunciation})\n${dino.info} It lived in the ${dino.period} period, over ${dino.mya[dino.mya.length - 1]} million years ago.`
+    }
+  }
+  // guard clause, return an error message if the ID is incorrect or not found ( aka dino = false still after our for loop )
+  // console.log(getDinosaurDescription(exampleDinosaurData, YLtkN9R3)) // test code
+  return "A dinosaur with an ID of 'incorrect-id' cannot be found."
+}
 
 /**
  * getDinosaursAliveMya()
@@ -71,7 +118,38 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
+function getDinosaursAliveMya(dinosaurs, mya, key) {
+  // initalize our accumulator var that we will use to return at the end
+  let dinos = []
+// our logic goes here
+// iterate over dinosaurs
+for (const dinosaur of dinosaurs) {
+  if(dinosaur.mya[0] >= mya && dinosaur.mya[1] <= mya){
+    // add id to our accumulator if there is no key param
+    if(key){
+      dinos.push(dinosaur[key])
+    } else {
+      dinos.push(dinosaur["dinosaurId"])
+    }
+  } else if(dinosaur.mya.length === 1){
+    if(dinosaur.mya[0] === mya || dinosaur.mya[0] - 1 === mya){
+      if(key){
+        dinos.push(dinosaur[key])
+      } else {
+        dinos.push(dinosaur['dinosaurID'])
+      }
+    }
+  }
+  }
+  return dinos // is this the same as the range for loop
+}
+// if mya param is between dinos myas
+// else if dinos mya.length is equal to 1
+  // if mya param is equal to dinos mya OR if mya param is equal to dinos mya - 1
+  // add id to our accumulator if there is no key param
+
+  // return accumulator 
+
 
 module.exports = {
   getLongestDinosaur,
